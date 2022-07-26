c6d66d6fc277fef189137758746cb3cc8cceacfe
TahaTasleem <83905957+TahaTasleem@users.noreply.github.com>
2022-07-26 17:33:36 +0500

    Add files via upload

---
+++ t6.sql
@@ -0,0 +1,10 @@
+--------------------------------------------------------
+--  File created - Tuesday-July-26-2022   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for View HPML_HR_ORGANIZATIONS
+--------------------------------------------------------
+
+  CREATE OR REPLACE FORCE EDITIONABLE VIEW "HPMLVMS"."HPML_HR_ORGANIZATIONS" ("ORGANIZATION_ID", "NAME") AS 
+  select org.organization_id, org.name from apps.HR_ALL_ORGANIZATION_UNITS_TL org
+;
